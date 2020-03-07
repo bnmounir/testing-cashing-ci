5786fb29f713b07d1d6fd4fa3051b8cf8c0687f3
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-03-07 10:45:12 -0500

    mongo uri 2nd fix

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 require('../models/User');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, {
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/blog_ci';
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
